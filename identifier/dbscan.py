0e30befed31587d1231ccbdecf1f2cabebcbdca0
Ramon <ramon@email.com>
2022-12-06 12:28:57 +0100

    Refactorized and added classifier

---
+++ identifier/dbscan.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+from sklearn.cluster import MiniBatchKMeans, DBSCAN
+from matplotlib import pyplot as plt
+from colorthief import ColorThief
+from mpl_toolkits import mplot3d
+
+def remove_background(imagen):
+    hsv = cv2.cvtColor(np.copy(imagen), cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, (36, 25, 25), (86, 255,255))
+    imask = mask==0
+    m = imask.astype("uint8")
+    target = cv2.bitwise_and(imagen, imagen, mask=m)
+    return target
+
+def quantize(img, clusters):
+    dominant_color = (0, 0, 0)
+    h, w = img.shape[:2]
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    image = image.reshape((image.shape[0] * image.shape[1], 3))
+    clf = MiniBatchKMeans(n_clusters=clusters)
+    labels = clf.fit_predict(image)
+    centers =  clf.cluster_centers_.astype("uint8")
+    colors = cv2.cvtColor(np.array([centers]), cv2.COLOR_LAB2RGB)[0]
+    for color in colors:
+        if color[0] > 3 and color[1] > 3 and color[2] > 3:
+            dominant_color = color
+    #cs, rep = np.unique(labels, return_counts=True)
+    quantized = centers[labels]
+    quantized = quantized.reshape((h, w, 3))
+    quantized = cv2.cvtColor(quantized, cv2.COLOR_LAB2RGB, cv2.CV_8U)
+    return quantized, dominant_color
+
+paths = ['media/players/arg1.jpg', 'media/players/arg2.jpg', 'media/players/arg3.jpg', 'media/players/arg4.jpg', 'media/players/arg5.jpg',
+        'media/players/hol1.jpg', 'media/players/hol2.jpg', 'media/players/hol3.jpg', 'media/players/hol4.jpg', 'media/players/hol5.jpg']
+clusters = 2
+path = 'media/players/{}.jpg'
+dest = "media\\players\\quantized1.jpg"
+
+colors = []
+r = []
+g = []
+b = []
+for i in range(0, 13):
+    image = cv2.imread(path.format(i))
+    image = remove_background(image)
+    image = image[int(image.shape[0]*0.05):int(image.shape[0]*0.6), :]
+    quantized, dom_color = quantize(image, clusters=clusters)
+    colors.append(dom_color)
+    r.append(dom_color[0])
+    g.append(dom_color[1])
+    b.append(dom_color[2])
+
+fig = plt.figure()
+ax = plt.axes(projection ='3d')
+ax.scatter(r, g, b, r)
+ax.set_title('3D line plot geeks for geeks')
+#plt.show()
+#cv2.imshow("a", np.hstack([image, quantized]))
+cv2.imwrite(dest, quantized)
+i =  cv2.cvtColor(cv2.imread(dest), cv2.COLOR_BGR2RGB)
+print(colors)
+clf = MiniBatchKMeans(n_clusters=2)
+clf = DBSCAN(eps=30, min_samples=0)
+labels = clf.fit_predict(colors)
+#centers =  clf.cluster_centers_.astype("uint8")
+print(labels)
+#cv2.waitKey(0)
--- translation/translation.py
+++ translation/translation.py
@@ -1,6 +1,14 @@
 import cv2
+from identifier.classifier import classify
+
+def sum(x, y, array):
+    return x*array[0] + y*array[1] + array[2]
+
+def compute(x, y, H):
+    return (sum(x, y, H[0])/sum(x,y,H[2])), (sum(x, y, H[1])/sum(x,y,H[2]))
 
 def get_players(results, image):
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     coord = results.xyxy[0]
     detecciones = coord.size(dim=0)
     puntos = []
@@ -11,25 +19,22 @@ def get_players(results, image):
         x2 = int(coord[obj][2].item()) 
         y2 = int(coord[obj][3].item())
         puntos.append([int(((x1 + x2)/2)), y2])
-        players.append(image[y1:y2, x1:x2])
+        player = image[y1:y2, x1:x2]
+        players.append(player)
+    classify(players, puntos)
     return puntos, players
-
-def sum(x, y, array):
-    return x*array[0] + y*array[1] + array[2]
-
-def compute(x, y, H):
-    return (sum(x, y, H[0])/sum(x,y,H[2])), (sum(x, y, H[1])/sum(x,y,H[2]))
 
 def pintar_puntos(img, puntos):
     for punto in puntos:
         x = int(punto[0])
         y = int(punto[1])
-        img = cv2.circle(img, (x, y), 3, (255, 0, 25), -1)
+        color = punto[2]
+        img = cv2.circle(img, (x, y), 3, color, -1)
     return img
 
 def draw_template(template, puntos, H):
     dst_points = []
     for punto in puntos:
         x, y = compute(punto[0], punto[1], H)
-        dst_points.append([int(x), int(y)])
+        dst_points.append([int(x), int(y), punto[2]])
     return pintar_puntos(template, dst_points)
